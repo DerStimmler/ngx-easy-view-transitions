bb35fe9f4e93203174ed24fac3427386c899b39c
Tim Schneider <43130816+DerStimmler@users.noreply.github.com>
2026-01-25 23:38:54 +0100

    feat: add hashing to transitionName, add preserveName input, add showOriginalNameAttr input

--- ngx-easy-view-transitions/src/lib/transition-name.directive.ts
+++ ngx-easy-view-transitions/src/lib/transition-name.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, effect, ElementRef, inject, input, Renderer2 } from '@angular/core';
+import { computed, Directive, effect, ElementRef, inject, input, Renderer2, untracked } from '@angular/core';
 import { KeyframesTransition } from './keyframes-transition';
 import { CssKeyframesTransition } from './css-keyframes-transition';
 import { KeyframesService } from './keyframes.service';
 import { ViewTransitionDirection, ViewTransitionsService } from './view-transitions.service';
-import { isValidViewTransitionName } from './utils';
+import { fnv1aHash, isValidViewTransitionName } from './utils';
 
 /**
  * Configure view transitions for the element
@@ -16,7 +16,7 @@ export class TransitionNameDirective {
   /**
    * Set the `view-transition-name` property to assign transitions to that element
    */
-  transitionName = input.required<string>();
+  originalTransitionName = input.required<string>({ alias: 'transitionName' });
   /**
    * Configure the animation when the element enters the view
    */
@@ -25,34 +25,67 @@ export class TransitionNameDirective {
    * Configure the animation when the element leaves the view
    */
   outAnimation = input<KeyframesTransition | CssKeyframesTransition>();
+  /**
+   * Whether to keep the original transition name or apply hashing for CSS safety.
+   * Defaults to `false`. Set to `true` to use the name as provided without hashing.
+   */
+  preserveName = input<boolean>(false);
+  /**
+   * Whether to show the `data-original-view-transition-name` containing the provided transition name. Useful for debugging when hashing is enabled. Defaults to `false`.
+   */
+  showOriginalNameAttr = input<boolean>(false);
 
   private readonly _el = inject(ElementRef);
   private readonly _renderer = inject(Renderer2);
   private readonly _keyframesService = inject(KeyframesService);
   private readonly _viewTransitionsService = inject(ViewTransitionsService);
 
+  private readonly _transitionName = computed(() => {
+    const originalTransitionName = this.originalTransitionName();
+
+    return this.preserveName() ? originalTransitionName : `vtn-${fnv1aHash(originalTransitionName)}`;
+  });
+
   constructor() {
     //transitionName
     effect(() => {
-      const transitionName = this.transitionName();
+      this._renderer.setStyle(this._el.nativeElement, 'view-transition-name', this._transitionName());
+    });
 
-      this._renderer.setStyle(this._el.nativeElement, 'view-transition-name', transitionName);
-
+    //originalNameAttr
+    effect(() => {
       queueMicrotask(() => {
-        if (!isValidViewTransitionName(transitionName))
-          console.warn(
-            `The transition name "${transitionName}" is potentially invalid. Please use a valid CSS <custom-ident> value and avoid forbidden values like "unset", "initial", "inherit" and "none".`
+        if (this.showOriginalNameAttr()) {
+          this._renderer.setAttribute(
+            this._el.nativeElement,
+            'data-original-view-transition-name',
+            untracked(this.originalTransitionName)
           );
+        }
       });
     });
 
+    //warn if invalid transition name
+    effect(() => {
+      queueMicrotask(() => {
+        if (!this.preserveName()) return;
+
+        const transitionName = this._transitionName();
+
+        if (isValidViewTransitionName(transitionName)) return;
+
+        console.warn(
+          `The provided transition name "${transitionName}" is potentially invalid. Please use a valid CSS <custom-ident> value and avoid forbidden values like "unset", "initial", "inherit" and "none". Or use hashing by setting "preserveName" to false.`
+        );
+      });
+    });
     //inAnimation
     effect(async () => {
       const animation = this.inAnimation();
 
       if (!animation) return;
 
-      this.setTransition(this.transitionName(), animation, 'in');
+      this.setTransition(this._transitionName(), animation, 'in');
     });
 
     //outAnimation
@@ -61,7 +94,7 @@ export class TransitionNameDirective {
 
       if (!animation) return;
 
-      this.setTransition(this.transitionName(), animation, 'out');
+      this.setTransition(this._transitionName(), animation, 'out');
     });
   }
 
